9226924f562f2a9df0c3960e7df1e438b4532ecc
Adroso360 <adrian.lapico@my.jcu.edu.au>
2016-09-18 21:37:55 +1000

    Debug 2

--- DebuggingExercises/FixDebugTwelve2.java
+++ DebuggingExercises/FixDebugTwelve2.java
@@ -1,7 +1,6 @@
 // A byte can't hold a value higher than 127
 // This program throws an ArithmeticException
 // if adding two bytes yields a value that is too high
-/*
 public class FixDebugTwelve2
 {
    public static void main(String[] args)
@@ -10,15 +9,15 @@ public class FixDebugTwelve2
       final byte HIGHBYTE = 127;
       try
       {
-         allowed = (byte)(HIGHBYTE - num1);
+         byte allowed = (byte) (HIGHBYTE - num1);
          if(num2 > allowed)
-            throw(new Arithmeticexception());
+            throw(new ArithmeticException());
          result = (byte)(num1 + num2);
          System.out.println("Result is " + result);
       }
       catch(ArithmeticException error)
       {
-         System.out.println("Byte can't hold value higher than " + HIGHBYT);
+         System.out.println("Byte can't hold value higher than " + HIGHBYTE);
       }
    }
-}*/
+}
